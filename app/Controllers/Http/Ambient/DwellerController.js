329483fe350eecd3cafd0ad98cfee406defdd1f4
Victor Mesquita <mesquitadev@gmail.com>
2019-10-26 21:26:32 -0300

    Finalizando Controller Dweller

--- app/Controllers/Http/Ambient/DwellerController.js
+++ app/Controllers/Http/Ambient/DwellerController.js
@@ -64,6 +64,15 @@ class DwellerController {
    * @param {View} ctx.view
    */
   async show ({ params, request, response, view }) {
+    const dweller = await Dweller.find(params.id);
+    if(dweller){
+      return response.json(dweller);
+    } else {
+      return response.status(401).json({
+        error : true,
+        message : "Morador não cadastrado!"
+      });
+    }
   }
 
   /**
@@ -75,6 +84,24 @@ class DwellerController {
    * @param {Response} ctx.response
    */
   async update ({ params, request, response }) {
+    const data = request.only([
+      'nome',
+      'telefone',
+      'apartment_id',
+      'status'
+    ]);
+    const dweller = await Dweller.find(params.id);
+    if(!dweller) {
+      return response.status(401).json({
+        message : 'Morador não encontrado!'
+      });
+    }
+    dweller.nome = data.nome;
+    dweller.telefone = data.telefone;
+    dweller.apartment_id = data.apartment_id;
+    dweller.status = data.status;
+    await dweller.save();
+    return response.status(201).json({success : true, message: 'Morador Atualizado com Sucesso!'});
   }
 
   /**
@@ -86,6 +113,16 @@ class DwellerController {
    * @param {Response} ctx.response
    */
   async destroy ({ params, request, response }) {
+    const dweller = await Dweller.find(params.id);
+    if(!dweller) {
+      return response.status(401).json({
+        message : "Erro! não encontrado!"
+      });
+    }
+    await dweller.delete();
+    return response.status(200).json({
+      message : "Removido com sucesso!"
+    });
   }
 }
 
