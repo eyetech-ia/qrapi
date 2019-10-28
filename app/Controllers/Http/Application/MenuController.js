dd739c863e6253109386a714bdfd0a8947777f53
Victor Mesquita <mesquitadev@gmail.com>
2019-10-28 09:37:12 -0300

    D - Adição Rota / e mensagem de Inicio

--- app/Controllers/Http/Application/MenuController.js
+++ app/Controllers/Http/Application/MenuController.js
@@ -1,4 +1,4 @@
-'use strict'
+'use strict';
 
 /** @typedef {import('@adonisjs/framework/src/Request')} Request */
 /** @typedef {import('@adonisjs/framework/src/Response')} Response */
--- start/routes.js
+++ start/routes.js
@@ -1,5 +1,5 @@
 'use strict';
-
+const env = use('Env');
 /*
 |--------------------------------------------------------------------------
 | Routes
@@ -16,7 +16,11 @@
 /** @type {typeof import('@adonisjs/framework/src/Route/Manager')} */
 const Route = use('Route');
 
-Route.on('/').render('welcome');
+Route.get('/', () => {
+  return {
+    message: 'Welcome to EyeTech IA Api, access the application in host:' + env.get('APP_URL')
+  }
+});
 
 
 Route.group(()=>{
